99edb1cd379d9069fa95fab9903db7862b88e6e0
seungjaeshanekim <seungjaeshanekim@gmail.com>
2018-07-28 17:45:34 +0900

    window 객체 오류 수정

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
-  // $(document).keyup(function (e) {
-  //   if (e.keyCode === 27) {
-  //     $('.grid').toggleClass('grid-show');
-  //   }
-  // });
-// window.resize(fuction(){
-//   location.reload();
+// $(document).keyup(function (e) {
+//   if (e.keyCode === 27) {
+//     $('.grid').toggleClass('grid-show');
+//   }
 // });
+$(window).resize(function () {
+  location.reload();
+});
 var viewport = window.matchMedia('(max-width: 999px)');
 
-if(viewport.matches){
+if (viewport.matches) {
   var nav = $('.navigation');
   var menu = $('.menu');
   var btn = $('<button class="btn-menu" aria-label="메인 메뉴 열기"></button>');
@@ -17,51 +17,41 @@ if(viewport.matches){
   var bar_bottom = $('<span class="menubar menubar-bottom"></span>');
   var item = $('.menu > li');
   var text = $('.menu-item-text');
-
   btn.append(bar_top, bar_middle, bar_bottom);
   nav.prepend(btn);
   item.addClass('menu-item');
-  item.attr('tabindex', "0");
+  item.attr('tabindex', '0');
   text.addClass('icon-plus');
-
 
   btn.click(function () {
     $(this).toggleClass('btn-menu-act');
     menu.toggleClass('menu-act');
     if ($(this).hasClass('btn-menu-act')) {
-      $(this).attr('aria-label', '메인 메뉴 닫기')
+      $(this).attr('aria-label', '메인 메뉴 닫기');
     } else {
-      $(this).attr("aria-label", "메인 메뉴 열기")
+      $(this).attr("aria-label", "메인 메뉴 열기");
     }
   });
 
-  item.on('click keyup', function(e){
-    if(e.type === 'click' || e.keyCode === 13 || e.keyCode === 32){
+  item.on('click keyup', function (e) {
+    if (e.type === 'click' || e.keyCode === 13 || e.keyCode === 32) {
       item.removeClass('menu-item-act');
       $(this).addClass('menu-item-act');
-      if($(this).hasClass('menu-item-act')){
+      if ($(this).hasClass('menu-item-act')) {
         console.log('하위메뉴 펼쳐져 있음');
-        text.removeClass('icon-minus') .addClass('icon-plus');
-        $(this).find(text) .removeClass('icon-plus') .addClass('icon-minus');
+        text.removeClass('icon-minus').addClass('icon-plus');
+        $(this).find(text).removeClass('icon-plus').addClass('icon-minus');
       }
     }
   });
-  
-  menu.focusin(function(){
+
+  menu.focusin(function () {
     btn.addClass('btn-menu-act');
     menu.addClass('menu-act');
-   });
-  menu.focusout(function(){
+  });
+
+  menu.focusout(function () {
     btn.removeClass('btn-menu-act');
     menu.removeClass('menu-act');
   });
-  
 }
-
-
-
-
-
-
-
-
